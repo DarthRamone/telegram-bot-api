537ffa49c91db8212d1b4afff688ce500715d4d2
Aleksandr Moshkin <darthramone@icloud.com>
2020-05-16 14:18:10 +0300

    round tripper

--- bot.go
+++ bot.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"os"
@@ -36,7 +37,22 @@ type BotAPI struct {
 //
 // It requires a token, provided by @BotFather on Telegram.
 func NewBotAPI(token string) (*BotAPI, error) {
-	return NewBotAPIWithClient(token, APIEndpoint, &http.Client{})
+	var t http.RoundTripper = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		MaxIdleConnsPerHost:   10,
+	}
+	c := &http.Client{Transport: t}
+	return NewBotAPIWithClient(token, APIEndpoint, c)
 }
 
 // NewBotAPIWithAPIEndpoint creates a new BotAPI instance
@@ -79,12 +95,13 @@ func (b *BotAPI) SetAPIEndpoint(apiEndpoint string) {
 func (bot *BotAPI) MakeRequest(endpoint string, params url.Values) (APIResponse, error) {
 	method := fmt.Sprintf(bot.apiEndpoint, bot.Token, endpoint)
 
-	log.Println("post form")
+	log.Printf("post form: %s", params)
 	resp, err := bot.Client.PostForm(method, params)
 	if err != nil {
 		return APIResponse{}, err
 	}
 	defer func() {
+		bot.Client.CloseIdleConnections()
 		err := resp.Body.Close()
 		log.Println(err)
 	}()
